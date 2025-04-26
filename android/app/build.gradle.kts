242dd1b6cbce94682493650ba80a5be1073d6878
Hamath Kane <kawkumputer@gmail.com>
2025-04-26 11:13:32 +0200

    update build version

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
         targetSdk = flutter.targetSdkVersion
-        versionCode = 4
-        versionName = "1.0.4"
+        versionCode = 6
+        versionName = "1.0.6"
     }
 
     signingConfigs {
